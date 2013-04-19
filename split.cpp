7c5b4252bc05c3fb8f4e9dc82ca839e5ddb8b820
kajiro <yota.kajiro.76@gmail.com>
2013-04-20 01:23:50 +0900

    size_tからstd::string::size_typeへ変更。こないだ気づいたバグ。

--- split.cpp
+++ split.cpp
@@ -3,8 +3,8 @@
 bool splitStringList(const std::string& str_list, const std::string& str_delimiter, std::vector<std::string>& vec_str)
 {
     if (("" == str_list) || ("" == str_delimiter)) return false;
-    size_t si_current = 0;
-    size_t si_found   = 0;
+    std::string::size_type si_current = 0;
+    std::string::size_type si_found   = 0;
     while (std::string::npos != (si_found = str_list.find(str_delimiter, si_current)))
     {
         vec_str.push_back(str_list.substr(si_current, si_found - si_current));
